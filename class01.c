5ec693e5fc5d2746fe5d28af0615aeaf4c132df5
Aamin Simmi Singh <aaminsimmisingh@gmail.com>
2024-12-14 14:14:22 +0530

    First Commit

---
+++ class01.c
@@ -0,0 +1,206 @@
+#include<stdio.h>
+// main(){
+
+//int main(){
+//     printf("hello world");
+//     return 0 ;
+// }
+
+// int main(){
+//     int number;
+//     int age;
+//     int price;
+//     return 0; 
+// }
+
+
+// int main(){
+
+//     int age = 29;
+//     float pi = 3.14;
+//     char percentage = '%';
+//     return 0 ; 
+// }
+
+
+
+// int main(){
+//     int a ,b;
+
+//     printf("enter a \n");
+//     scanf("%d",&a);
+
+//     printf("enter b \n");
+//     scanf ("%d",&b);
+//     printf("sum of a and b is :%d\n",a+b);
+
+//     return 0;
+//     }
+
+
+// int main(){
+//     float radius;
+//     printf("enter the radius %f \n");
+//     scanf("%f",&radius);
+//     printf("area of a circle :%f\n", 3.14 * radius * radius );
+//     return 0;
+// }
+
+
+
+// int main(){
+//     int age = 22;
+//     int oldAge = age;
+//     int newAge = oldAge + 2;
+//     printf("new age is : %d",newAge);
+
+
+//     int rupee = 1, dollar;
+//     dollar = 74;  
+// }
+
+
+
+
+/*
+order of declaration is important - Wrong Declaration Order
+float pi = 3.14;
+float area = pi * rad * rad;
+float rad = 3;
+*/
+
+// // valid declaration
+// int age1, age2, age3;
+// age1 = age2 = age3 = 22;
+// //invalid
+// //int a1 = a2 = a3 = 22;
+// return 0;
+// }
+
+
+
+// 2. Arithmetic Instructions
+
+// int main() {
+// int a = 1, b = 2, c = 3;
+// //valid
+// a = b + c;
+// //invalid
+// // b + c = a;
+
+
+// printf("%d \n" , 3 % 2);
+// printf("%d \n" , -3 % 2);
+// return 0;
+// }
+
+
+//     printf("sum of 2 & 3 : %d" , 2 +3 );
+//     printf("sum of 2.0 & 3 : %f " , 2.0 + 3);
+//     printf("sum of 2.0 and 3.0 : %f", 2.0 +3.0 );
+//     return 0 ;
+// } 
+    // printf("Output : %d", 5+2/2*3);
+    // }
+
+//     printf("%d \n",4<3);
+//     printf("%d \n",3<4);
+//     printf("%d \n", 4<4);
+//     printf("%d \n",4>=4);
+//     printf("%d \n",4<=4);
+    
+//     printf("%d \n", 4>3);
+// printf("%d \n", 3>4);
+// printf("%d \n", 4>4);
+// printf("%d \n", 4>=4);
+// printf("%d \n", 4!=4);
+// printf("%d \n", 3!=4);
+// return 0;
+// }
+    
+    
+
+//     printf("%d \n", 3<4 && 3<5);
+// printf("%d \n", 3<4 && 5<4);
+// printf("%d \n", 3<4 && 5<4);
+// printf("%d \n", 3>4 && 5>4);
+// printf("%d \n", 3<4 && 3<5);
+// printf("%d \n", !(3<4 && 3<5));
+// printf("%d \n", !(4<3 || 5<3));
+// return 0;
+// }
+
+
+//     int a = 10;
+//     a+= 10;
+//     printf("a+10 = %d \n" , a);
+//     a-= 10;
+//     printf("a-10 = %d \n" , a);
+//     a *= 10;
+//     printf("a* 10 = %d\n" , a);
+//     a/= 10;
+//     printf("a / 10 = %d\n" , a);
+//     a %= 10;
+//         printf("a % c10 = %d\n" , a);
+//    return 0;
+// }
+  
+    
+    // int i,n;
+    //  int n1 = 0;
+    //  int n2 = 1;
+    //  int nextSum = n1 + n2;
+
+
+    //  printf("Enter number of terms: ");
+    //  scanf("%d", &n);
+    //  printf("Fibonacci Series: %d,%d," ,n1,n2);
+
+    //  for(int i = 3; i<=n; i++){
+    //      printf("%d",nextSum);
+    //      n1 = n2 ;
+//         n2 = nextSum;
+//         nextSum = n1 + n2;
+
+//      }
+//      return 0;
+    // }
+
+
+    /*int a,b,c,large;
+    printf("enter three numbers  :");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b){
+        if(a>c)
+        large = a;
+        else 
+        large = c;
+
+    }
+    else {
+        if (b>c)
+        large = b;
+        else 
+        large = c;
+    }
+     printf("largest number is %d\n",large);
+     }*/
+
+
+
+/*void main(){
+    int age ;
+    printf("Enter age :\n");
+    scanf("%d",&age);
+    if(age >= 18){
+        printf("eligible for voting \n");
+    }
+        else {
+        printf("not eligible for voting\n");
+        
+    } getch();
+
+}*/
+
+
+
